d62f534a4130efe1fb67b762afc6681527ff17fc
brokiem <milyhqvideo@gmail.com>
2023-03-09 08:00:04 +0800

    Use medium quality audio

--- utils/audio.js
+++ utils/audio.js
@@ -6,7 +6,9 @@ import { leave_voice_channel } from "./utils.js";
 export async function play_audio(input, guild_id, voice_channel_id, is_queue) {
   prepare_voice_connection(guild_id, voice_channel_id);
 
-  const options = {};
+  const options = {
+    quality: 1
+  };
   let video_info = null;
 
   if (input.startsWith("https")) {
@@ -53,6 +55,7 @@ export async function play_audio(input, guild_id, voice_channel_id, is_queue) {
 export async function seek_audio(guild_id, timeSeconds) {
   const stream = await playdl.stream(client.streams[guild_id].yt_url, {
     seek: timeSeconds,
+    quality: 1,
   });
 
   await broadcast_audio(guild_id, stream);
